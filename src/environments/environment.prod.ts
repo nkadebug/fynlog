509ce2d79e34257aed2f2beb916809bb08f63592
nkadebug <nka.debug@gmail.com>
2021-04-06 05:56:23 +0000

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAqClRn4VTjJXjpkcOlDxNkPseespG1pxc",
+    authDomain: "fynlog.firebaseapp.com",
+    projectId: "fynlog",
+    storageBucket: "fynlog.appspot.com",
+    messagingSenderId: "137527553879",
+    appId: "1:137527553879:web:f2a12a10489565fa9f5812",
+    measurementId: "G-0EG3RG5XXD"
+  }
 };
